db4e8c5d8a277bad52df8798edb0d4c6858afe75
3mrfouad <3mrfouad@gmail.com>
2021-10-11 23:21:31 -0600

    use grid

--- tutorial.py
+++ tutorial.py
@@ -5,8 +5,10 @@ root = Tk()
 # everything else is widget
 
 # create a label that goest into the window
-myLabel = Label(root, text = "Hello World!")
+myLabel1 = Label(root, text = "Hello World!")
+myLabel2 = Label(root, text = "Hello Grid")
 # shoving it into the root window
-myLabel.pack()
+myLabel1.grid(row = 0, column =0)
+myLabel2.grid(row = 1, column =5)
 # create event loop to monitor the continus changes
 root.mainloop()
