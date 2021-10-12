a10bf198bfe4207991a631ca6adf9c0fe177790b
3mrfouad <3mrfouad@gmail.com>
2021-10-11 23:41:44 -0600

    create entry box

--- tutorial.py
+++ tutorial.py
@@ -3,15 +3,19 @@ from tkinter import *
 # create the root window
 root = Tk()
 # everything else is widget
-# create button
-
-
 def handleClick():
-    myLabel = Label(root, text="Button Clicked")
+    myLabel = Label(root, text=f"Hello {e.get()}")
     myLabel.pack()
+  
+# input box
+myLabel = Label(root, text="Enter your name: ")
+myLabel.pack()
 
+e = Entry(root, width=50, bg="black", fg="white", borderwidth=3)
+e.insert(0, "Your Name:") # default text not a placeholder
+e.pack()
 
-myBtn = Button(root, text="Button", fg="white", bg="black",
+myBtn = Button(root, text="Ok", fg="white", bg="black",
                padx=40, pady=40, command=handleClick)
 myBtn.pack()
 root.mainloop()
