40baf8118bc469aeef10f9e500f0d2ddab0228da
3mrfouad <3mrfouad@gmail.com>
2021-10-12 01:41:38 -0600

    add decimal and sign buttons w/ functionality

--- calc.py
+++ calc.py
@@ -57,6 +57,19 @@ def button_divide():
     e.delete(0, END)
 
 
+def button_sign():
+    current = -1 * get_num()
+    e.delete(0, END)
+    e.insert(0, f"{current}")
+
+
+def button_decimal():
+    current : str = e.get()
+    if not current.__contains__('.'):
+        e.delete(0, END)
+        e.insert(0, f"{current + '.'}")
+
+
 def button_equal():
     second_operand = get_num()
     e.delete(0, END)
@@ -82,23 +95,25 @@ button_2 = Button(root, text="2", padx=40, pady=20,
                   command=lambda: button_click(button_2['text']))
 button_3 = Button(root, text="3", padx=40, pady=20,
                   command=lambda: button_click(button_3['text']))
-
 button_4 = Button(root, text="4", padx=40, pady=20,
                   command=lambda: button_click(button_4['text']))
 button_5 = Button(root, text="5", padx=40, pady=20,
                   command=lambda: button_click(button_5['text']))
 button_6 = Button(root, text="6", padx=40, pady=20,
                   command=lambda: button_click(button_6['text']))
-
 button_7 = Button(root, text="7", padx=40, pady=20,
                   command=lambda: button_click(button_7['text']))
 button_8 = Button(root, text="8", padx=40, pady=20,
                   command=lambda: button_click(button_8['text']))
 button_9 = Button(root, text="9", padx=40, pady=20,
                   command=lambda: button_click(button_9['text']))
-
 button_0 = Button(root, text="0", padx=40, pady=20,
                   command=lambda: button_click(button_0['text']))
+
+button_decimal = Button(root, text=".", padx=42, pady=20,
+                        command=button_decimal)
+button_sign = Button(root, text="+/-", padx=34, pady=20,
+                     command=button_sign)
 
 button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
 button_subtract = Button(root, text="-", padx=40,
@@ -107,28 +122,26 @@ button_multiply = Button(root, text="*", padx=40,
                          pady=20, command=button_multiply)
 button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
 
-button_equal = Button(root, text="=", padx=40, pady=20, command=button_equal)
+button_equal = Button(root, text="=", padx=87, pady=20, command=button_equal)
 button_clear = Button(root, text="Clear", padx=76,
                       pady=20, command=button_clear)
-
 
 # put buttons on screen
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
 button_3.grid(row=3, column=2)
-
 button_4.grid(row=2, column=0)
 button_5.grid(row=2, column=1)
 button_6.grid(row=2, column=2)
-
 button_7.grid(row=1, column=0)
 button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
+button_0.grid(row=4, column=1)
 
-button_0.grid(row=4, column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
-
-button_equal.grid(row=5, column=3)
+button_sign.grid(row=4, column=0)
+button_decimal.grid(row=4, column=2)
+button_clear.grid(row=5, column=0, columnspan=2)
+button_equal.grid(row=5, column=2, columnspan=2)
 
 button_add.grid(row=4, column=3)
 button_subtract.grid(row=3, column=3)
